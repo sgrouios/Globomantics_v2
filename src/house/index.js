ae6f932ca244b8b8b2bcb404e6f89de7d11f6bf5
Stephen Grouios <s.grouios@hotmail.com>
2022-06-24 11:33:24 +1000

    Add Inquiry component

--- src/house/index.js
+++ src/house/index.js
@@ -1,6 +1,14 @@
 import './house.css';
+import { useState } from 'react';
+import emailIcon from './Email.png';
+import Inquiry from './Inquiry';
 
 const House = ({ house }) => {
+    const [inquiryShown, setInquiryShown] = useState(false);
+    const inquiryClick = () => {
+        setInquiryShown(!inquiryShown)
+    }
+
     return (
         <div>
             <div className='row mt-2'>
@@ -16,6 +24,13 @@ const House = ({ house }) => {
                 <div className='col-md-5'>
                     <p className='price'>${house.price}</p>
                     <p>{house.description}</p>
+                    <img 
+                        src={emailIcon}
+                        height="50"
+                        alt="inquiry"
+                        onClick={inquiryClick}
+                    />
+                    {inquiryShown && <Inquiry />}
                 </div>
             </div>
         </div>
